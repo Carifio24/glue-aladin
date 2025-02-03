2e44182fcf362b27f36c449481bd07e8f93c431f
Carifio24 <carifio24@gmail.com>
2025-02-03 17:45:11 -0500

    Start working on viewer tests.

--- glue_aladin/tests/test_viewer.py
+++ glue_aladin/tests/test_viewer.py
@@ -1,6 +1,8 @@
 from glue_qt.app import GlueApplication
 from glue.core import Data
 
+from glue_aladin.tests.utils import assert_expression_value
+
 from ..data_viewer import AladinLiteViewer
 
 
@@ -13,6 +15,21 @@ class TestAladinLiteViewer(object):
         self.dc.append(self.d)
         self.hub = self.dc.hub
         self.session = self.application.session
-
-    def test_basic(self):
         self.viewer = self.application.new_data_viewer(AladinLiteViewer)
+        self.options = self.viewer.options_widget()
+
+    def register(self):
+        self.viewer.register_to_hub(self.hub)
+
+    def test_add_data(self):
+        self.viewer.add_data(self.d)
+        self.viewer.state.ra_att = self.d.id['ra']
+        self.viewer.state.dec_att = self.d.id['dec']
+        assert len(self.viewer.layers) == 1
+
+    def test_center(self):
+        self.viewer.add_data(self.d)
+        self.viewer.state.ra_att = self.d.id['ra']
+        self.viewer.state.dec_att = self.d.id['dec']
+        self.viewer.layers[0].center()
+        assert_expression_value(self.viewer.aladin_widget, "aladin.getRaDec();", "[1, 1]")
